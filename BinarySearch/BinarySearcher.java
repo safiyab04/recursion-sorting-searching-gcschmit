1d2ee346f9b5c5a7cd2fdacb57851d1d7ab18ecc
Matt Callaghan <mcallaghan@naperville203.org>
2020-03-12 09:50:42 -0500

    Initial commit

---
+++ BinarySearch/BinarySearcher.java
@@ -0,0 +1,21 @@
+/**
+   A class for executing binary searches in an array.
+*/
+public class BinarySearcher
+{  
+   /**
+      Finds a value in a range of a sorted array, using the binary
+      search algorithm.
+      @param a the array in which to search
+      @param low the low index of the range
+      @param high the high index of the range
+      @param value the value to find
+      @return the index at which the value occurs, or -1
+      if it does not occur in the array
+   */
+   public static int search(int[] a, int low, int high, int value)
+   {  
+      
+   }
+}
+
