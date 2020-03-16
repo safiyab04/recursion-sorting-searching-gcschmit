50c13666572a18597eb536e9b9945c29573e96f3
Geoff Schmit <gcschmit@gmail.com>
2020-03-16 10:58:05 -0500

    updated to read from a file; refactored to be recursive

--- PalindromeTester/PalindromeTester.java
+++ PalindromeTester/PalindromeTester.java
@@ -1,4 +1,6 @@
 import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
 
 /**
  * Demonstrates the use of nested while loops.
@@ -11,37 +13,59 @@ public class PalindromeTester
      * Tests strings to see if they are palindromes.
      *
      */
-   public static void main (String[] args)
+   public static void main (String[] args) throws FileNotFoundException
    {
-      String str, another = "y";
-      int left, right;
-      Scanner s = new Scanner(System.in);
+      String str = "";
+      String fileName = "palindrome.txt";
 
-      do
+      File inputFile = new File(fileName);
+      Scanner s = new Scanner(inputFile);
+
+      // make everything that isn't a letter, a delimiter
+      s.useDelimiter("[^A-Za-z]+");
+
+      /* read the entire file, one word at a time, and concatenate each work into str */
+      while(s.hasNext())
       {
-         System.out.println ("Enter a potential palindrome:");
-         str = s.nextLine();
-
-         left = 0;
-         right = str.length() - 1;
-
-         while (str.charAt(left) == str.charAt(right) && left < right)
-         {
-            left++;
-            right--;
-         }
-
-         System.out.println();
-
-         if (left < right)
-            System.out.println ("That string is NOT a palindrome.");
-         else
-            System.out.println ("That string IS a palindrome.");
-
-         System.out.println();
-         System.out.print ("Test another palindrome (y/n)? ");
-         another = s.nextLine();
+          str += s.next();
       }
-      while (another.equalsIgnoreCase("y")); // allows y or Y
+
+      s.close();
+
+      // make the entire string lowercase
+      str = str.toLowerCase();
+
+      System.out.println(str);
+
+      if(isPalindrome(str))
+      {
+          System.out.println ("That string IS a palindrome.");
+      }
+      else
+      {
+          System.out.println ("That string is NOT a palindrome.");
+      }
+   }
+
+   public static boolean isPalindrome(String str)
+   {
+       // must have a terminating case
+       if(str.length() <= 1)
+       {
+           return true;
+       }
+
+       // solve a small part of the problem
+       String firstChar = str.substring(0, 1);
+       String lastChar = str.substring(str.length() - 1);
+       if(firstChar.equals(lastChar))
+       {
+           // recurse with a simpler version of the problem
+           return isPalindrome(str.substring(1, str.length() - 1));
+       }
+       else
+       {
+           return false;
+       }
    }
 }
